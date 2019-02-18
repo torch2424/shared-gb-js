023f3c2ae6aae26bef58328a7ca4b7970ba22166
Aaron Turner <aaron@aaronthedev.com>
2019-02-18 01:52:24 -0800

    Added prettier fixed roms

--- googleDrivePicker.js
+++ googleDrivePicker.js
@@ -1,12 +1,12 @@
 // API For loading GB Roms from Google Drive
 // Using the Picker API
 
-import loadScript from 'load-script';
+import loadScript from "load-script";
 
 // Google drive
-const GOOGLE_SDK_URL = 'https://apis.google.com/js/api.js';
+const GOOGLE_SDK_URL = "https://apis.google.com/js/api.js";
 // OAuth Key for Google Drive
-let googlePickerOAuthToken = '';
+let googlePickerOAuthToken = "";
 
 class GoogleDrivePickerLib {
   constructor() {
@@ -19,14 +19,18 @@ class GoogleDrivePickerLib {
 
     // Load the Picker SDK
     loadScript(GOOGLE_SDK_URL, () => {
-      window.gapi.load('auth');
-      window.gapi.load('picker');
+      window.gapi.load("auth");
+      window.gapi.load("picker");
     });
   }
 
   getFile(mimeTypes) {
-    if (!this._isGoogleReady() || !this._isGoogleAuthReady() || !this._isGooglePickerReady()) {
-      return Promise.reject('Google Drive not ready');
+    if (
+      !this._isGoogleReady() ||
+      !this._isGoogleAuthReady() ||
+      !this._isGooglePickerReady()
+    ) {
+      return Promise.reject("Google Drive not ready");
     }
 
     const token = window.gapi.auth.getToken();
@@ -42,7 +46,9 @@ class GoogleDrivePickerLib {
     if (oauthToken) {
       this._createPicker(oauthToken, mimeTypes, getFilePromiseObject);
     } else {
-      this._doAuth(({ access_token }) => this._createPicker(access_token, mimeTypes, getFilePromiseObject));
+      this._doAuth(({ access_token }) =>
+        this._createPicker(access_token, mimeTypes, getFilePromiseObject)
+      );
     }
 
     return getFilePromise;
@@ -64,7 +70,7 @@ class GoogleDrivePickerLib {
     window.gapi.auth.authorize(
       {
         client_id: this.clientId,
-        scope: ['https://www.googleapis.com/auth/drive.readonly'],
+        scope: ["https://www.googleapis.com/auth/drive.readonly"],
         immediate: false
       },
       callback
@@ -74,11 +80,11 @@ class GoogleDrivePickerLib {
   _createPicker(oauthToken, mimeTypes, getFilePromiseObject) {
     googlePickerOAuthToken = oauthToken;
 
-    const googleViewId = google.picker.ViewId['DOCS'];
+    const googleViewId = google.picker.ViewId["DOCS"];
     const view = new window.google.picker.DocsView(googleViewId);
 
     if (mimeTypes) {
-      view.setMimeTypes(mimeTypes.join(','));
+      view.setMimeTypes(mimeTypes.join(","));
     }
 
     if (!view) {
@@ -97,8 +103,14 @@ class GoogleDrivePickerLib {
     // Calculate our picker size
     // https://stackoverflow.com/questions/1248081/get-the-browser-viewport-dimensions-with-javascript
     // https://developers.google.com/picker/docs/reference#PickerBuilder
-    const viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    const viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    const viewportWidth = Math.max(
+      document.documentElement.clientWidth,
+      window.innerWidth || 0
+    );
+    const viewportHeight = Math.max(
+      document.documentElement.clientHeight,
+      window.innerHeight || 0
+    );
     picker.setSize(parseInt(viewportWidth, 10), parseInt(viewportHeight, 10));
 
     picker.build().setVisible(true);
@@ -106,13 +118,13 @@ class GoogleDrivePickerLib {
 
   _pickerCallback(data, getFilePromiseObject) {
     const loadGoogleDriveFileTask = async () => {
-      if (data.action === 'cancel') {
+      if (data.action === "cancel") {
         getFilePromiseObject.resolve({ cancelled: true });
         return;
       }
 
       // We only want the picked action
-      if (data.action !== 'picked') {
+      if (data.action !== "picked") {
         return;
       }
 
@@ -122,13 +134,17 @@ class GoogleDrivePickerLib {
 
       const googlePickerFileObject = data.docs[0];
       const oAuthHeaders = new Headers({
-        Authorization: 'Bearer ' + googlePickerOAuthToken
+        Authorization: "Bearer " + googlePickerOAuthToken
       });
 
       // First Fetch the Information about the file
-      const responseJson = await fetch('https://www.googleapis.com/drive/v2/files/' + googlePickerFileObject.id, {
-        headers: oAuthHeaders
-      })
+      const responseJson = await fetch(
+        "https://www.googleapis.com/drive/v2/files/" +
+          googlePickerFileObject.id,
+        {
+          headers: oAuthHeaders
+        }
+      )
         .then(response => {
           return response.json();
         })
--- openSourceROMs/preactComponents.js
+++ openSourceROMs/preactComponents.js
@@ -1,28 +1,29 @@
 // Need h for rendering the elements in an object
-import { h } from 'preact';
+import { h } from "preact";
 
 // Import our ROMs
-import {GameROMs, DemoROMs, ToolROMs} from './urlImports';
+import { GameROMs, DemoROMs, ToolROMs } from "./urlImports";
 
 // Import our ROM images
-import tobuTobuGirlImage from './ROMs/tobutobugirl/tobutobugirl.png';
-import backToColorImage from './ROMs/back-to-color/back-to-color.png';
-import libbetImage from './ROMs/libbet/libbet.png';
-import dinosOfflineAdventureImage from './ROMs/dinos-offline-adventure/dinos-offline-adventure.png';
-import waveformImage from './ROMs/waveform/waveform.png';
+import tobuTobuGirlImage from "./ROMs/tobutobugirl/tobutobugirl.png";
+import backToColorImage from "./ROMs/back-to-color/back-to-color.png";
+import libbetImage from "./ROMs/libbet/libbet.png";
+import dinosOfflineAdventureImage from "./ROMs/dinos-offline-adventure/dinos-offline-adventure.png";
+import waveformImage from "./ROMs/waveform/waveform.png";
 
 export const openSourceROMs = {
   games: {
     tobutobugirl: {
-      title: 'tobu tobu girl',
+      title: "tobu tobu girl",
       url: GameROMs.TobuTobuGirl,
       image: tobuTobuGirlImage,
-      link: 'http://tangramgames.dk/tobutobugirl/',
+      link: "http://tangramgames.dk/tobutobugirl/",
       infoElement: (
         <div>
           <p>
-            Tobu Tobu Girl is a fun and challenging arcade platformer developed by Tangram Games featuring an original soundtrack by
-            potato-tan. Licensed under MIT/CC-BY.
+            Tobu Tobu Girl is a fun and challenging arcade platformer developed
+            by Tangram Games featuring an original soundtrack by potato-tan.
+            Licensed under MIT/CC-BY.
           </p>
         </div>
       )
@@ -31,31 +32,42 @@ export const openSourceROMs = {
       title: "Dino's Offline Adventure",
       url: GameROMs.DinosOfflineAdventure,
       image: dinosOfflineAdventureImage,
-      link: 'https://gaming-monster.itch.io/dinos-offline-adventure',
+      link: "https://gaming-monster.itch.io/dinos-offline-adventure",
       infoElement: (
         <div>
           <p>
-            Dino's Offline Adventure is a port of the modern classic built into Chrome the web browser.
-
-            Built as our first Gameboy game and developed using the Gameboy Development Kit (GBDK) this simple game is even more of a challenge on the smaller Gameboy screen.
+            Dino's Offline Adventure is a port of the modern classic built into
+            Chrome the web browser. Built as our first Gameboy game and
+            developed using the Gameboy Development Kit (GBDK) this simple game
+            is even more of a challenge on the smaller Gameboy screen.
           </p>
           <p>
-            P.S <a href="https://www.youtube.com/channel/UCMMhSfBStEti-Lqzs30HYWw" target="_blank">The developer has an awesome Game Boy Development Youtube Channel</a>
+            P.S{" "}
+            <a
+              href="https://www.youtube.com/channel/UCMMhSfBStEti-Lqzs30HYWw"
+              target="_blank"
+            >
+              The developer has an awesome Game Boy Development Youtube Channel
+            </a>
           </p>
         </div>
       )
     },
     libbet: {
-      title: 'Libbet and the Magic Floor',
+      title: "Libbet and the Magic Floor",
       url: GameROMs.Libbet,
       image: libbetImage,
-      link: 'https://github.com/pinobatch/libbet',
+      link: "https://github.com/pinobatch/libbet",
       infoElement: (
         <div>
           <p>
-            This is a port of Martin Korth's game Magic Floor to the Game Boy compact video game system, introducing Libbet the boulder girl.
-
-            The floor tiles have four shades. Libbet can roll or jump between tiles of the same shade. She can also roll or jump onto the next brighter shade or from white to black, which leaves a track. Tiles with no exits contain a trap door that leads to the entrance. Leave 90 percent of possible tracks and roll to the exit to win.
+            This is a port of Martin Korth's game Magic Floor to the Game Boy
+            compact video game system, introducing Libbet the boulder girl. The
+            floor tiles have four shades. Libbet can roll or jump between tiles
+            of the same shade. She can also roll or jump onto the next brighter
+            shade or from white to black, which leaves a track. Tiles with no
+            exits contain a trap door that leads to the entrance. Leave 90
+            percent of possible tracks and roll to the exit to win.
           </p>
         </div>
       )
@@ -63,47 +75,46 @@ export const openSourceROMs = {
   },
   demos: {
     backToColor: {
-      title: 'Back to Color',
+      title: "Back to Color",
       url: DemoROMs.BackToColor,
       image: backToColorImage,
-      link: 'https://github.com/AntonioND/back-to-color',
+      link: "https://github.com/AntonioND/back-to-color",
       infoElement: (
         <div>
-          <p>Back to Color, a GBC demo for the GBDev 2014 compo. Made by AntonioND.</p>
+          <p>
+            Back to Color, a GBC demo for the GBDev 2014 compo. Made by
+            AntonioND.
+          </p>
         </div>
       )
     }
   },
   tools: {
     waveform: {
-      title: 'waveform',
+      title: "waveform",
       url: ToolROMs.waveform,
       image: waveformImage,
-      link: 'https://github.com/dannye/waveform-gb',
+      link: "https://github.com/dannye/waveform-gb",
       infoElement: (
         <div>
           <p>
-            This program visualizes the wave form used by the wave channel.
-            The wave form can be edited freely and playback of the wave is updated immediately.
-
-            Up to 8 custom wave forms can be saved.
-            There are 8 built-in wave forms that can be loaded at any time.
+            This program visualizes the wave form used by the wave channel. The
+            wave form can be edited freely and playback of the wave is updated
+            immediately. Up to 8 custom wave forms can be saved. There are 8
+            built-in wave forms that can be loaded at any time.
           </p>
         </div>
       )
     }
-
   }
 };
 
 export const getOpenSourceROMElements = loadROMCallback => {
-  
   const openSourceROMSections = [];
   Object.keys(openSourceROMs).forEach(romTypeKey => {
-
     const openSourceROMElements = [];
-    Object.keys(openSourceROMs).forEach(romKey => {
-      const openSourceROM = openSourceROMs[romKey];
+    Object.keys(openSourceROMs[romTypeKey]).forEach(romKey => {
+      const openSourceROM = openSourceROMs[romTypeKey][romKey];
       openSourceROMElements.push(
         <div class="open-source-rom">
           <button
@@ -117,12 +128,23 @@ export const getOpenSourceROMElements = loadROMCallback => {
             </div>
             <div class="open-source-rom__right">
               <h3>{openSourceROM.title}</h3>
-              <div class="open-source-rom__info">{openSourceROM.infoElement}</div>
+              <div class="open-source-rom__info">
+                {openSourceROM.infoElement}
+              </div>
             </div>
           </button>
-          <a href={openSourceROM.link} target="blank_" class="open-source-rom__link">
+          <a
+            href={openSourceROM.link}
+            target="blank_"
+            class="open-source-rom__link"
+          >
             {/*Google Material Link Icon*/}
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
               <path d="M0 0h24v24H0z" fill="none" />
               <path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z" />
             </svg>
@@ -131,14 +153,16 @@ export const getOpenSourceROMElements = loadROMCallback => {
       );
     });
 
-    openSourceROMSections.push((
+    openSourceROMSections.push(
       <div>
-        <h1>{romTypeKey.charAt(0).toUpperCase()}{romTypeKey.slice(1)}</h1>
+        <h1>
+          {romTypeKey.charAt(0).toUpperCase()}
+          {romTypeKey.slice(1)}
+        </h1>
         {openSourceROMElements}
       </div>
-    ));
+    );
   });
-
 
   return openSourceROMSections;
 };
